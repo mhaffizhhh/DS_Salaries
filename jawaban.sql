7eedc4dc68d1e0b10560b21a6e7b39cddf325088
mhaffizhhh <140355758+mhaffizhhh@users.noreply.github.com>
2023-08-03 16:24:08 +0700

    Update jawaban.sql

--- jawaban.sql
+++ jawaban.sql
@@ -73,6 +73,28 @@ from ds_year
 		on ds_year.work_year = ds_2.work_year
 	left join ds_3
 		on ds_year.work_year = ds_3.work_year;
+
+-- Use RANK()
+with expect as ( 
+select 
+	work_year,
+    job_title,
+    rank() over(partition by work_year order by avg(salary_in_usd) desc) as ranks,
+    avg(salary_in_usd) as avg_sal
+from
+	ds_salaries
+group by
+	work_year,
+    job_title
+)
+select
+	work_year,
+    job_title,
+    avg_sal
+from
+	expect
+where
+	ranks = 1;
         
 
 -- 3. Country where the average salary is greater than total average salary.
@@ -127,6 +149,27 @@ left join ds_2
 on ds_year.work_year = ds_2.work_year
 left join ds_3
 on ds_year.work_year = ds_3.work_year;
+
+-- Use RANK()
+with expect as(
+select
+	work_year,
+    job_title,
+    salary_in_usd,
+    rank() over(partition by work_year order by salary_in_usd desc) as ranks
+from
+	ds_salaries
+where
+ job_title like "%data analyst%" and employment_type = "ft"
+)
+select
+	distinct work_year,
+    job_title,
+    salary_in_usd
+from
+	expect
+where
+	ranks = 1;
 
 -- 5. Most highly salary from each year for %data analyst% and FT.
 select job_title, (avg(salary_in_usd) * 15000)/12 as avg_monthly_rp
